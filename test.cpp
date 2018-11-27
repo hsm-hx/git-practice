118738961303e7971b852b3644e6a482d0d32ff6
hsm-hx <skytea922@gmail.com>
2018-11-27 13:46:34 +0900

    :heavy_plus_sign: test.cppを修正 #30
    :heavy_plus_sign: test.cppを修正 #30

--- test.cpp
+++ test.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int sum(int a, int b){
-  return a+b;
+int diff(int a, int b){
+  return a-b;
 }
 
 int main(void){
-  int n;
+  int a, b;
 
-  cin >> n;
+  cin >> a >> b;
 
-  cout << n << endl;
+  cout << diff(a, b) << endl;
 
   return 0;
 }
